b0da8f7812033ff792f40ddb5d218391e009897b
Ydnar21 <98470255+Ydnar21@users.noreply.github.com>
2025-04-01 13:28:08 -0400

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    port: 8080,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    assetsDir: 'assets',
+  }
 });
