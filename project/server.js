525829dce1f1ad7ffe0903a7149d204f77568604
Ydnar21 <98470255+Ydnar21@users.noreply.github.com>
2025-04-01 14:20:48 -0400

    Update server.js

--- project/server.js
+++ project/server.js
@@ -36,6 +36,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
